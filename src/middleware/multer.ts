dcc715e91c6ca8ed319d9479f99cbbc30330325f
FOKO KENMOGNE WILFRIED <fokowilfried7@gmail.com>
2025-05-19 15:25:27 +0100

    update: remove unused comment

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,19 +1,17 @@
 import multer from 'multer';
 import path from 'path';
 
-// Storage config for images
 const imageStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images/');
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        const ext = path.extname(file.originalname); // File extension
-        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`); // File name
+        const ext = path.extname(file.originalname);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
     }
 });
 
-// Image filter
 const imageFilter = (req: Express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
     if (!allowedTypes.includes(file.mimetype)) {
@@ -22,10 +20,9 @@ const imageFilter = (req: Express.Request, file: Express.Multer.File, cb: multer
     cb(null, true);
 };
 
-// Multer configuration
 const uploadImage = multer({
     storage: imageStorage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5Mo file size limit
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: imageFilter,
 });
 
